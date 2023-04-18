ae37854b3313e3d3ea440a7a78640c4b869d64c0
Vaibhav Jaiswal <vaibhav.09047@gmail.com>
2023-04-18 20:19:25 +0530

    Type String Boolean Number complete

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,22 +1,69 @@
 import styles from "./Main.module.css";
 import TypeSBN from "../Type_SBN/Type_SBN";
-
+import { useState } from "react";
 const Main = () => {
+  const [fields, setFields] = useState([
+    { name: "order", type: "STRING", required: true },
+  ]);
+  const addNewField = () => {
+    setFields((fields) => {
+      const newFields = [...fields];
+      newFields.push({ name: "order", type: "STRING", required: true });
+      return newFields;
+    });
+  };
+  const updateFields = (id, name, type, required) => {
+    setFields((fields) => {
+      const newFields = [...fields];
+      newFields[id] = { name, type, required };
+      return newFields;
+    });
+  };
+  const deleteField = (id) => {
+    setFields((fields) => {
+      const newFields = [...fields];
+      newFields.splice(id, 1);
+      return newFields;
+    });
+  };
+  const printToConsole = () => {
+    console.log(fields);
+  };
   return (
-    <div className={styles["container"]}>
-      <div className={styles["main"]}>
-        <div className={styles["main__header"]}>
-          <span>Field name and type</span>
-          <button className={styles["main__header__add"]}>+</button>
-        </div>
-        <div className={styles["main__body"]}>
-          <ol>
-            <li>
-              <TypeSBN />
-            </li>
-          </ol>
+    <div className={styles["main-container"]}>
+      <div className={styles["container"]}>
+        <div className={styles["main"]}>
+          <div className={styles["main__header"]}>
+            <span>Field name and type</span>
+            <button
+              onClick={addNewField}
+              className={styles["main__header__add"]}
+            >
+              +
+            </button>
+          </div>
+          <div className={styles["main__body"]}>
+            <ol>
+              {fields.map((field, index) => (
+                <li key={index}>
+                  <TypeSBN
+                    updateFields={updateFields}
+                    deleteField={deleteField}
+                    key={index}
+                    id={index}
+                    name={field.name}
+                    type={field.type}
+                    required={field.required}
+                  />
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
       </div>
+      <button onClick={printToConsole} className={styles.main__btn}>
+        Print
+      </button>
     </div>
   );
 };
--- src/components/Type_SBN/Type_SBN.js
+++ src/components/Type_SBN/Type_SBN.js
@@ -1,25 +1,29 @@
 import { useState, useEffect } from "react";
 import styles from "./Type_SBN.module.css";
 import { Switch } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
 const TypeSBN = (props) => {
-  const [value, setValue] = useState("order");
+  const [value, setValue] = useState(`${props.name}`);
   const [focus, setFocus] = useState(false);
-  const [required, setRequired] = useState(true);
+  const [required, setRequired] = useState(props.required);
+  const [type, setType] = useState(props.type);
   const onFocus = (e) => {
     setFocus(true);
-    console.log(e.target.value);
   };
   const onChange = (e) => {
     setValue(e.target.value);
   };
   const onBlur = (e) => {
     setFocus(false);
-    console.log(e.target.value);
   };
   const onChangeRequired = (e) => {
     setRequired((prev) => !prev);
   };
+  const onTypeChange = (e) => {
+    setType(e.target.value);
+  };
+  useEffect(() => {
+    props.updateFields(props.id, value, type, required);
+  }, [value, required, type]);
   return (
     <div className={styles.main}>
       <div className={styles.main__left}>
@@ -36,6 +40,8 @@ const TypeSBN = (props) => {
           onChange={onChange}
         ></input>
         <select
+          value={type}
+          onChange={onTypeChange}
           name="datatype"
           id="datatype"
           className={styles.main__left__select}
@@ -55,7 +61,10 @@ const TypeSBN = (props) => {
           size="small"
         />
         {/* <button className={styles["main__right__add"]}>+</button> */}
-        <div className={styles["main__right__img__container"]}>
+        <div
+          onClick={() => props.deleteField(props.id)}
+          className={styles["main__right__img__container"]}
+        >
           <img src="/delete.png"></img>
         </div>
       </div>
